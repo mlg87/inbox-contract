6eddd33788ec27f2ba9bb80484a4762bd7766f4b
Mason <masonlgoetz@gmail.com>
2018-03-08 00:10:44 -0700

    updated basic tests for Inbox Contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,23 +1,41 @@
 const assert = require('assert')
 const ganache = require('ganache-cli')
 const Web3 = require('web3')
-const web3 = new Web3(ganache.provider())
 const { interface, bytecode } = require('../compile')
+
+const provider = ganache.provider()
+const web3 = new Web3(provider)
 
 let accounts
 let inbox
+const INITIAL_MESSAGE = 'init message'
+const NEW_MESSAGE = 'new message'
+
 // deploy contract before each test
 beforeEach(async () => {
   // get list of all accounts
   accounts = await web3.eth.getAccounts()
   // use one account to deploy contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Init message'] })
+    .deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
     .send({ from: accounts[0], gas: '1000000' })
+
+  inbox.setProvider(provider)
 })
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    console.log('inbox', inbox)
+    assert.ok(inbox.options.address)
+  })
+
+  it('sets the initial message to: init message', async () => {
+    const message = await inbox.methods.message().call()
+    assert.equal(message, INITIAL_MESSAGE)
+  })
+
+  it('updates the value of message when setMessage is called', async () => {
+    await inbox.methods.setMessage(NEW_MESSAGE).send({ from: accounts[0] })
+    const newMessage = await inbox.methods.message().call()
+    assert.equal(newMessage, NEW_MESSAGE)
   })
 })
